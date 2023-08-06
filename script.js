2051d6d4146ffeb6eb179c424a0e44b51a85fcb1
Eugene <68537036+eugeneplug@users.noreply.github.com>
2023-08-06 18:15:45 +0300

    v1

---
+++ script.js
@@ -0,0 +1,44 @@
+import { Player } from "./player.js";
+
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+let player;
+
+startGame();
+
+function startGame() {
+    init();
+    animate();
+}
+
+function init(){
+    const movementLimits = {
+        minX: 0,
+        maxX: canvas.width,
+        minY: 0,
+        maxY: canvas.height,
+    };
+
+    player = new Player(canvas.width/2, canvas.height/2, context, movementLimits);    
+};
+
+
+
+
+
+
+
+
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    player.update()
+}
